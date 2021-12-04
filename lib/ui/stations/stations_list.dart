a576ec159d89491b34a4336d6454874db51133d5
Charise Walraven <cherri.walraven@gmail.com>
2021-12-04 17:04:49 +0100

    create stations list widget with check for image validity

---
+++ lib/ui/stations/stations_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:radio_app/core/providers/app_state.dart';
+import 'package:radio_app/model/station_stream/station_stream.dart';
+import 'package:radio_app/ui/widgets/tile.dart';
+
+class StationList extends StatelessWidget {
+  const StationList({
+    Key? key,
+    required this.stations,
+  }) : super(key: key);
+
+  final List<StationStream> stations;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppState>(
+      builder: (context, state, child) {
+        return ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            itemCount: stations.length,
+            itemBuilder: (BuildContext context, int index) {
+              StationStream station = stations[index];
+              return Tile(
+                  title: station.name,
+                  onTap: () {
+                    debugPrint('Title tapped: ${station.name}');
+                    state.playStream(station.name, station.urlResolved == "" ? station.url: station.urlResolved);
+                  },
+                  imageUrl: _isValidFaviconUrl(station.favicon)? station.favicon: ""
+              );
+            }
+        );
+      }
+    );
+  }
+
+  bool _isValidFaviconUrl(String url) {
+    RegExp re = RegExp(r'(\.png|\.jpg|\.ico)');
+    return re.hasMatch(url);
+  }
+}
