9948d22578938ed47e23c2d9da6f42175189892d
Charise Walraven <cherri.walraven@gmail.com>
2021-12-04 17:02:28 +0100

    create basic home page

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:radio_app/ui/player/player_bar.dart';
+import 'package:radio_app/ui/stations/stations_view.dart';
+import 'package:radio_app/ui/widgets/bottom_bar.dart';
+import 'package:radio_app/ui/widgets/tile.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomBar(),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  StationsView(),
+                  PlayerBar(),
+                ],
+            ),
+          ),
+        ),
+    );
+  }
+}
