29b749e3c280d2ccdca73441bce1656442b347b8
Charise Walraven <cherri.walraven@gmail.com>
2021-12-08 22:30:33 +0100

    Adds LocalStationsState Provider; Formats code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:radio_app/core/providers/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:radio_app/core/providers/local_stations_state.dart';
 import 'package:radio_app/core/providers/stations_state.dart';
 
 import 'radio_app.dart';
@@ -9,12 +10,19 @@ void main() async {
   runApp(
     //instantiate the RadioPlayerState class which holds the state for the entire time the app is running
     //Everything in this state class is now universally available wherever you have context.
-     MultiProvider(
-       providers: [
-         ChangeNotifierProvider<AppState>(create: (context) => AppState()),
-         ChangeNotifierProvider<StationsListState>(create: (context) => StationsListState()),
-       ],
-       child: const RadioApp(),
-     ),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppState>(
+          create: (context) => AppState(),
+        ),
+        ChangeNotifierProvider<StationsState>(
+          create: (context) => StationsState(),
+        ),
+        ChangeNotifierProvider<LocalStationsState>(
+          create: (_) => LocalStationsState(),
+        ),
+      ],
+      child: const RadioApp(),
+    ),
   );
 }
