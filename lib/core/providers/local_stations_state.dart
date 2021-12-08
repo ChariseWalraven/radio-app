98f867181893f5fe8211d687a6284ab9aeee5be1
Charise Walraven <cherri.walraven@gmail.com>
2021-12-08 22:43:47 +0100

    imports station_stream_filter

--- lib/core/providers/local_stations_state.dart
+++ lib/core/providers/local_stations_state.dart
@@ -1,3 +1,4 @@
 import 'package:radio_app/core/providers/stations_state.dart';
+import 'package:radio_app/model/station_stream/station_stream_filter.dart';
 
 class LocalStationsState extends StationsState {}
