4015703a491ed02efba5abe97695afceefbede0a
Charise Walraven <cherri.walraven@gmail.com>
2021-12-08 22:45:10 +0100

    Overrides init, changes filter; Woohoo!

--- lib/core/providers/local_stations_state.dart
+++ lib/core/providers/local_stations_state.dart
@@ -4,4 +4,9 @@ import 'package:radio_app/model/station_stream/station_stream_filter.dart';
 class LocalStationsState extends StationsState {
   final StationStreamFilter _filter = StationStreamFilter(countrycode: "NL");
 
+  @override
+  init() {
+    super.filter = _filter;
+    super.init();
+  }
 }
