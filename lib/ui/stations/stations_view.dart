378ec3bcc776130443a46ad7d8b79be1466ce4e4
Charise Walraven <cherri.walraven@gmail.com>
2021-12-13 17:35:00 +0100

    converts StationsView to stateful widget

--- lib/ui/stations/stations_view.dart
+++ lib/ui/stations/stations_view.dart
@@ -5,9 +5,17 @@ import 'package:radio_app/model/station_stream/station_stream_filter.dart';
 import 'package:radio_app/ui/stations/local_stations.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
-class StationsView extends StatelessWidget {
-  StationsView({Key? key}) : super(key: key);
+class StationsView extends StatefulWidget {
+  const StationsView({Key? key}) : super(key: key);
 
+  /// The alignment to be used next time the user scrolls or jumps to an item.
+  static double alignment = 0;
+
+  @override
+  State<StationsView> createState() => _StationsViewState();
+}
+
+class _StationsViewState extends State<StationsView> {
   /// Listener that reports the position of items when the list is scrolled.
   final ItemPositionsListener itemPositionsListener =
   ItemPositionsListener.create();
