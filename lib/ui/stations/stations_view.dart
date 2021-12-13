f5eb23b2ba43535939d6455a5af7cecc4d9dd731
Charise Walraven <cherri.walraven@gmail.com>
2021-12-13 17:58:07 +0100

    Did some stuff to make it work; Gotta go now, improv

--- lib/ui/stations/stations_view.dart
+++ lib/ui/stations/stations_view.dart
@@ -4,11 +4,13 @@ import 'package:provider/src/provider.dart';
 import 'package:radio_app/core/abstract_classes/order.dart';
 import 'package:radio_app/core/providers/local_stations_state.dart';
 import 'package:radio_app/model/station_stream/station_stream_filter.dart';
+import 'package:radio_app/services/location_service.dart';
 import 'package:radio_app/ui/stations/local_stations.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class StationsView extends StatefulWidget {
   const StationsView({Key? key}) : super(key: key);
+
 
   @override
   State<StationsView> createState() => _StationsViewState();
@@ -18,20 +20,22 @@ class _StationsViewState extends State<StationsView> {
   /// Listener that reports the position of items when the list is scrolled.
   final ItemPositionsListener itemPositionsListener =
   ItemPositionsListener.create();
+  final LocationService _locationService = LocationService();
+  String? _countryCode;
+
+  Future<String?> getCountryCode() async {
+    await _locationService.getLocation();
+    _countryCode = await _locationService.getCountryCode();
+    debugPrint('countryCode: $_countryCode');
+    return _countryCode;
+  }
 
   @override
   void initState() {
     super.initState();
     // TODO: use country code from phone instead
     // TODO: create a flow for obtaining permission to access country code
-    
-    StationStreamFilter filter = StationStreamFilter(
-      countrycode: "GB", 
-      limit: 10, 
-      order: Order.clickCount, 
-      reverse: true
-    );
-    context.read<LocalStationsState>().setInitialStations(filter, notify: true);
+  
   }
 
   @override
@@ -40,9 +44,26 @@ class _StationsViewState extends State<StationsView> {
       padding: const EdgeInsets.all(12.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const <Widget>[
-          LocalStations(),
-          SizedBox(
+        children: <Widget>[
+          FutureBuilder<String?>(
+            future: getCountryCode(),
+            builder: (context, snapshot) {
+              if(snapshot.hasData && snapshot.data != null) {
+                StationStreamFilter filter = StationStreamFilter(
+                  countrycode: snapshot.data ?? "GB",
+                  limit: 10, 
+                  order: Order.clickCount, 
+                  reverse: true
+                );
+
+                context.read<LocalStationsState>().setInitialStations(filter, notify: true);
+                return const LocalStations();
+              } else {
+                return const CircularProgressIndicator();
+              }
+            }
+          ) ,
+          const SizedBox(
             child: Text('Coming soon!!'),
           )
         ],
