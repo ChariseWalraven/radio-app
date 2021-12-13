4405ef611770d1e6907e76012a92a0c6961f51e0
Charise Walraven <cherri.walraven@gmail.com>
2021-12-13 16:24:25 +0100

    comment out filter override

--- lib/core/providers/local_stations_state.dart
+++ lib/core/providers/local_stations_state.dart
@@ -3,11 +3,11 @@ import 'package:radio_app/core/providers/stations_state.dart';
 import 'package:radio_app/model/station_stream/station_stream_filter.dart';
 
 class LocalStationsState extends StationsState {
-  final StationStreamFilter _filter = StationStreamFilter(countrycode: "NL", order: Order.clickCount);
+  // final StationStreamFilter _filter = StationStreamFilter(countrycode: "NL", order: Order.clickCount);
 
-  @override
-  init() {
-    super.filter = _filter;
-    super.init();
-  }
+  // @override
+  // init() {
+  //   super.filter = _filter;
+  //   super.init();
+  // }
 }
