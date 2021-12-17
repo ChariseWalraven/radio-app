2121a4bbab466e7165fc4e0a3119984669ddb39a
Charise Walraven <cherri.walraven@gmail.com>
2021-12-17 19:37:52 +0100

    adds localizations delegates

--- lib/radio_app.dart
+++ lib/radio_app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:radio_app/core/constants/constants.dart';
 import 'package:radio_app/ui/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,14 @@ class RadioApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appName,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const <Locale>[
+        Locale('en', ''), // English, no country code
+      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
