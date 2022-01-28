649238d9f4c7274fb993cb4150a2d1c778b6abe3
Charise Walraven <cherri.walraven@gmail.com>
2022-01-28 16:19:36 +0100

    removes unused import and code

--- lib/ui/station/stations_collections.dart
+++ lib/ui/station/stations_collections.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:radio_app/core/providers/stations_state.dart';
-import 'package:radio_app/model/station/station.dart';
 import 'package:radio_app/ui/station/stations_collection.dart';
 
 class StationsCollections extends StatelessWidget {
-  StationsCollections({Key? key}) : super(key: key);
-
-  // final StationCollection _stationCollection = StationCollection(
-  //   title: "Test stations",
-  //   filter: StationsFilter(limit: 10, hidebroken: true,)
-  // );
-
-  // final _models.StationsCollection _stationsCollection = _models.StationsCollection();
+  const StationsCollections({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -34,23 +26,3 @@ class StationsCollections extends StatelessWidget {
     );
   }
 }
-
-/*
-_stationsCollection.stationCollectionList.map((stationCollection) => 
-        FutureBuilder(
-          future: stationCollection.stations,
-          builder: (context, AsyncSnapshot<List<Station>> snapshot) {
-            if(snapshot.hasData) {
-              debugPrint('${snapshot.data!.length}');
-              return StationsCollection(
-              title: '${stationCollection.title}',
-              stations: snapshot.data ?? [],
-              );
-            }
-            else {
-              return const CircularProgressIndicator();
-            }
-          }
-        )
-      ).toList()
-*/
