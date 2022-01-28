a9b89e1a800e0878edb337f27c561e28dcc2f2de
Charise Walraven <cherri.walraven@gmail.com>
2022-01-28 14:49:09 +0100

    removes unused localizationsDelegates

--- lib/radio_app.dart
+++ lib/radio_app.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:radio_app/core/constants/constants.dart';
 import 'package:radio_app/ui/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -11,14 +10,6 @@ class RadioApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: appName,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const <Locale>[
-        Locale('en', ''), // English, no country code
-      ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
