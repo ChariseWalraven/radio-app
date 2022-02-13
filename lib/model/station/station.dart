97067a0fad107f0d213923ac423e22c80d6f39ff
Charise Walraven <cherri.walraven@gmail.com>
2022-02-13 18:44:27 +0100

    adds uuid to Station model

--- lib/model/station/station.dart
+++ lib/model/station/station.dart
@@ -1,38 +1,39 @@
 import 'dart:convert';
 
 class Station {
-    Station({
-        required this.name,
-        required this.url,
-        required this.urlResolved,
-        required this.homepage,
-        required this.favicon,
-        required this.tags,
-        required this.bitrate,
-        required this.clickCount,
-    });
+  Station({
+    required this.uuid,
+    required this.name,
+    required this.url,
+    required this.urlResolved,
+    required this.homepage,
+    required this.favicon,
+    required this.tags,
+    required this.bitrate,
+    required this.clickCount,
+  });
 
-    String name;
-    String url;
-    String urlResolved;
-    String homepage;
-    String favicon;
-    String tags;
-    int bitrate;
-    int clickCount;
+  String uuid;
+  String name;
+  String url;
+  String urlResolved;
+  String homepage;
+  String favicon;
+  String tags;
+  int bitrate;
+  int clickCount;
 
-    factory Station.fromRawJson(String str) =>
-        Station.fromJson(json.decode(str));
+  factory Station.fromRawJson(String str) => Station.fromJson(json.decode(str));
 
-    factory Station.fromJson(Map<String, dynamic> json) =>
-        Station(
-            name: json["name"].trim(),
-            url: json["url"].trim(),
-            urlResolved: json["url_resolved"].trim(),
-            homepage: json["homepage"].trim(),
-            favicon: json["favicon"].trim(),
-            tags: json["tags"].trim(),
-            bitrate: json["bitrate"],
-            clickCount: json["clickcount"],
-        );
+  factory Station.fromJson(Map<String, dynamic> json) => Station(
+        uuid: json["uui"].trim(),
+        name: json["name"].trim(),
+        url: json["url"].trim(),
+        urlResolved: json["url_resolved"].trim(),
+        homepage: json["homepage"].trim(),
+        favicon: json["favicon"].trim(),
+        tags: json["tags"].trim(),
+        bitrate: json["bitrate"],
+        clickCount: json["clickcount"],
+      );
 }
