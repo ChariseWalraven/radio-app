8f51253892b3f20d0e522ac3fa01b04c885ab8bf
Charise Walraven <cherri.walraven@gmail.com>
2022-02-13 20:46:27 +0100

    cleans up main; adds new state to provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:radio_app/core/providers/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:radio_app/core/providers/local_stations_state.dart';
+import 'package:radio_app/core/providers/favourites_state.dart';
 import 'package:radio_app/core/providers/stations_state.dart';
-// import 'package:radio_app/services/favourites_service.dart';
 
 import 'radio_app.dart';
 
 void main() {
-  // FavouritesService favoritesService = FavouritesService();
   runApp(
     MultiProvider(
       providers: [
@@ -18,8 +16,8 @@ void main() {
         ChangeNotifierProvider<StationsState>(
           create: (context) => StationsState(),
         ),
-        ChangeNotifierProvider<LocalStationsState>(
-          create: (_) => LocalStationsState(),
+        ChangeNotifierProvider<FavouritesState>(
+          create: (_) => FavouritesState(),
         ),
       ],
       child: const RadioApp(),
