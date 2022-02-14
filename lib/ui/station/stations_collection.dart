0932c8d80757235901d94824c8728ffba8eda103
Charise Walraven <cherri.walraven@gmail.com>
2022-02-14 19:17:34 +0100

    refactors for favourites page

--- lib/ui/station/stations_collection.dart
+++ lib/ui/station/stations_collection.dart
@@ -9,45 +9,68 @@ class StationsCollection extends StatelessWidget {
     Key? key,
     required this.stations,
     required this.title,
+    this.scrollDirection = Axis.horizontal,
   }) : super(key: key);
 
   final List<Station> stations;
   final String title;
-
+  final Axis scrollDirection;
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
-        Text(title),
-        SizedBox(
-          height: 180,
-          child: ListView.builder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            itemCount: stations.length,
-            itemBuilder: (BuildContext context, int index) {
-              Station station = stations[index];
-              return Tile(
-                  title: station.name,
-                  onTap: () {
-                    debugPrint('Title tapped: ${station.name}');
-                    context.read<AppState>().playStream(station.name, station.urlResolved == "" ? station.url: station.urlResolved);
-                  },
-                  imageUrl: _isValidFaviconUrl(station.favicon)? station.favicon: ""
-              );
-            }
-          ),
+        Flexible(
+          fit: FlexFit.loose,
+          child: Text(title)
         ),
+        scrollDirection == Axis.vertical
+            ? Flexible(
+                fit: FlexFit.loose,
+                flex: 20,
+                child: gridViewBuilder(),
+              )
+            : LimitedBox(
+                maxHeight: 180,
+                child: gridViewBuilder(),
+              ),
       ],
     );
   }
-
 
 // TODO: find a way to check if the file is empty (flutter throws an error if you try to display an empty file)
   bool _isValidFaviconUrl(String url) {
     RegExp re = RegExp(r'(\.png|\.jpg)');
     return re.hasMatch(url);
+  }
+
+  GridView gridViewBuilder() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: scrollDirection == Axis.vertical? 2 : 1,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20, 
+      ),
+      shrinkWrap: true,
+      scrollDirection: scrollDirection,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      itemCount: stations.length,
+      itemBuilder: (BuildContext context, int index) {
+        Station station = stations[index];
+        return Tile(
+            title: station.name,
+            onTap: () {
+              debugPrint('Title tapped: ${station.name}');
+              context.read<AppState>().playStream(
+                  station.name,
+                  station.urlResolved == ""
+                      ? station.url
+                      : station.urlResolved);
+            },
+            imageUrl:
+                _isValidFaviconUrl(station.favicon) ? station.favicon : "");
+      },
+    );
   }
 }
