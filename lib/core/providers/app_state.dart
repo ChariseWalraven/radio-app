902980dfd1a618e123cd009a8e97eecd740fd87b
Charise Walraven <cherri.walraven@gmail.com>
2022-02-14 21:32:31 +0100

    adds favourites functionality to radio player state

--- lib/core/providers/app_state.dart
+++ lib/core/providers/app_state.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'package:radio_app/core/constants/constants.dart';
 import 'package:radio_app/core/enums/playing_state.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:radio_app/model/station/station.dart';
+import 'package:radio_app/services/favourites_service.dart';
 
 //This is the state manager class for the entire app (stuff that needs to be accessed through the whole app)
 class AppState extends ChangeNotifier {
@@ -14,11 +17,11 @@ class AppState extends ChangeNotifier {
   PlayingState _playingState = PlayingState.none;
   PlayingState get playingState => _playingState;
 
-  String _station = 'No station selected';
-  String get station => _station;
+  Station _station = kDefaultStation;
+  Station get station => _station;
 
-  String _title = '';
-  String get title => _title;
+  // String _title = '';
+  // String get title => _title;
 
   AppState() {
     _init();
@@ -45,31 +48,31 @@ class AppState extends ChangeNotifier {
     if (_player.playing && _player.icyMetadata != null) {
       _what = _player.icyMetadata?.info?.title ?? '';
     }
-    if (_what != _title) {
-      _title = _what;
-      debugPrint('RadioPlayerState._getWhatson.changed=$_title');
+    if (_what != _station.name) {
+      // _station = 
+      // debugPrint('RadioPlayerState._getWhatson.changed=${_station.name}');
       notifyListeners();
     }
   }
 
-  Future<void> playStream(String newStation, String url) async {
+  Future<void> playStream(Station newStation) async {
     if (newStation == _station) {
       return;
     }
-    debugPrint('RadioPlayerState.playStream=$newStation');
+    // debugPrint('RadioPlayerState.playStream=$newStation');
     _station = newStation;
-    _title = '';
+    // _title = '';
     _setPlayingState(PlayingState.loading); //contains a notifyListeners call
     try {
       if (_player.playing) {
         await _player.stop();
       }
-      await _player.setUrl(url);
+      await _player.setUrl(_station.urlResolved);
       await _player.load();
       await startPlaying();
     } catch (e) {
       debugPrint('RadioPlayerState.playStream :: ERROR :: $e');
-      _station = 'Error: Cannot play $newStation';
+      // _station = 'Error: Cannot play ${newStation.name}';
       // TODO: remove broken stream
       // StreamService.removeStreamByName(newStation);
       _setPlayingState(PlayingState.none);
@@ -86,7 +89,7 @@ class AppState extends ChangeNotifier {
       } catch (e) {
         debugPrint('RadioPlayerState.startPlaying :: ERROR :: $e');
         _setPlayingState(PlayingState.none);
-        _station = 'Error trying to play $station';
+        // _station = 'Error trying to play $station';
       }
     }
   }
@@ -112,6 +115,22 @@ class AppState extends ChangeNotifier {
   // begin bottomBar code
   void updateSelectedIndex(int newIndex) {
     _selectedIndex = newIndex;
+    notifyListeners();
+  }
+
+  void toggleFavourite(String uuid) async {
+    debugPrint('AppState::toggleFavourite');
+    FavouritesService favService = FavouritesService();
+    if(_station.isFavourite) {
+      debugPrint('Removing favourite for uuid: $uuid');
+      await favService.removeFavourite(uuid);
+    }
+    else {
+      debugPrint('Adding favourite for uuid: $uuid');
+      await favService.addFavourite(uuid);
+    }
+    _station.toggleFavourite();
+    
     notifyListeners();
   }
 }
