14f8c1390deae0dc66241a55c9def5c9d83b02bb
Charise Walraven <cherri.walraven@gmail.com>
2022-02-14 19:14:06 +0100

    renames to StationsCollectionService for clarity; adds functionality for favourites

--- lib/services/stations_collection_service.dart
+++ lib/services/stations_collection_service.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/foundation.dart';
+import 'package:radio_app/core/constants/constants.dart';
 import 'package:radio_app/model/station/station.dart';
 import 'package:radio_app/model/station/stations_filter.dart';
+import 'package:radio_app/services/favourites_service.dart';
 import 'package:radio_app/services/stations_service.dart';
 
 // this class is responsible for keeping track of the stations associated with the filter and title passed to it
+// it can also handle favourite stations collections
 
-class StationsCollection {
-  StationsCollection({required this.title, required this.filter}) {
+class StationsCollectionService {
+  StationsCollectionService(
+      {required this.title,
+      this.filter = kDefaultStationsFilter,
+      this.isFavouritesList = false}) {
     _init();
   }
 
-  String title = "";
+  final String title;
   List<Station> collection = [];
-  
-  StationsFilter filter = StationsFilter();
+  final bool isFavouritesList;
+
+  StationsFilter filter = kDefaultStationsFilter;
+
+  final FavouritesService _favouritesService = FavouritesService();
 
   get stations => _fetchStations();
 
@@ -24,6 +33,15 @@ class StationsCollection {
 
   Future<List<Station>> _fetchStations() async {
     debugPrint('StationsCollection::_fetchStations');
-    return await StationsService.getStreams(filter); 
+
+    if (isFavouritesList) {
+      List<String> uuids = await _favouritesService.getFavourites();
+      return await StationsService.getStreamsByStationUUID(uuids);
+    }
+    return await StationsService.getStreams(filter);
+  }
+
+  Future<List<Station>> refreshStations() async {
+    return await _fetchStations();
   }
 }
