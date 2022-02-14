2679494b8e290c2e2287d7f1cfa1617b1aef1f67
Charise Walraven <cherri.walraven@gmail.com>
2022-02-14 19:16:00 +0100

    renames to favourites_view. first working version

---
+++ lib/ui/favourites/favourites_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:radio_app/core/providers/favourites_state.dart';
+import 'package:radio_app/model/station/station.dart';
+import 'package:radio_app/ui/station/stations_collection.dart';
+
+class FavouritesView extends StatelessWidget {
+  const FavouritesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<FavouritesState>(
+      builder: (BuildContext context, FavouritesState state, Widget? child) {
+        List<Station> favourites = state.favourites;
+        debugPrint('state: ${favourites.length}');
+        return StationsCollection(
+            stations: favourites,
+            title: "Favourites",
+            scrollDirection: Axis.vertical,
+            );
+      },
+    );
+  }
+}
