012470005a2964c92fc8723fb0a7f7b63437e75d
Charise Walraven <cherri.walraven@gmail.com>
2022-02-14 19:13:08 +0100

    first working version of favourites state

--- lib/core/providers/favourites_state.dart
+++ lib/core/providers/favourites_state.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/cupertino.dart';
+import 'package:radio_app/model/station/station.dart';
 import 'package:radio_app/services/favourites_service.dart';
+import 'package:radio_app/services/stations_collection_service.dart';
 
 class FavouritesState extends ChangeNotifier {
   final FavouritesService _favouritesService = FavouritesService();
+  final StationsCollectionService _stationsCollectionService = StationsCollectionService(title: "Favourite Stations", isFavouritesList: true);
   
-  List<String> _favourites = [];
-  List<String> get favourites => _favourites;
+  List<Station> _favourites = [];
+  List<Station> get favourites => _favourites;
 
   FavouritesState() {
     _init();
   }
 
   _init() async {
-    _favourites = await _favouritesService.getFavourites();
+    _favourites = await _stationsCollectionService.refreshStations();
     notifyListeners();
   }
 
   // TODO: method for add favourite, remove favourite
   addFavourite(String favourite) async {
-    _favourites = await _favouritesService.addFavourite(favourite);
-    notifyListeners();
+    await _favouritesService.addFavourite(favourite);
+    refreshFavourites();
   }
 
   removeFavourite(String favourite) async {
-    _favourites = await _favouritesService.removeFavourite(favourite);
+    _favourites = await _stationsCollectionService.refreshStations();
+    refreshFavourites();
+  }
+
+  Future<void> refreshFavourites({bool shouldNotifyListeners = true}) async {
+    _favourites = await _stationsCollectionService.refreshStations();
+    if(!shouldNotifyListeners) return;
     notifyListeners();
   }
 
