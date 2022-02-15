00bdabf54eaaed42c271de836f72d83d3e3f1286
Charise Walraven <cherri.walraven@gmail.com>
2022-02-15 17:53:10 +0100

    fix animation stuff for bottom bar; move routing to service

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -4,5 +4,3 @@ import 'package:radio_app/model/station/stations_filter.dart';
 const String kAppName = "Radio App";
 const StationsFilter kDefaultStationsFilter = StationsFilter();
 Station kDefaultStation = Station(stationuuid: "", name: "", url: "", urlResolved: "", homepage: "", favicon: "", tags: "", bitrate: 0, clickCount: 0);
-const String kFavouritesRouteName = "/favourites";
-const String kHomeRouteName =  "/";
