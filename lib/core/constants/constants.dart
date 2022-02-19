e63ba97d3f1f7ffd6fbbd417c6b5adeb158bf3aa
Charise Walraven <cherri.walraven@gmail.com>
2022-02-19 01:04:08 +0100

    adds soms icon sizes

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -4,3 +4,6 @@ import 'package:radio_app/model/station/stations_filter.dart';
 const String kAppName = "Radio App";
 const StationsFilter kDefaultStationsFilter = StationsFilter();
 Station kDefaultStation = Station(stationuuid: "", name: "", url: "", urlResolved: "", homepage: "", favicon: "", tags: "", bitrate: 0, clickCount: 0);
+const double kIconSizeSmall = 24.0;
+const double kIconSizeDefault = 32.0;
+const double kIconSizeLarge = kIconSizeSmall * 2;
