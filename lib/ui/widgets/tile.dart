31c5d9daecdcb95668f3939257c06440d451fa92
Charise Walraven <cherri.walraven@gmail.com>
2022-02-19 01:20:37 +0100

    tweaks ui of tile

--- lib/ui/widgets/tile.dart
+++ lib/ui/widgets/tile.dart
@@ -6,44 +6,84 @@ class Tile extends StatelessWidget {
     Key? key,
     required this.title,
     required this.onTap,
+    this.height = 120.0,
+    this.width = 120.0,
     this.imageUrl = "",
+    this.enableCustomBackground = false,
   }) : super(key: key);
 
+  final double height;
+  final double width;
   final String title;
   final String imageUrl;
   final VoidCallback onTap;
+  final bool enableCustomBackground;
 
   @override
   Widget build(BuildContext context) {
+    Widget _withCustomBackground = CustomCard(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: CoverImage(
+              imageUrl: imageUrl,
+            ),
+          ),
+          Expanded(
+            child: Text(title),
+          )
+        ],
+      ),
+    );
+
+    Widget _withoutCustomBackground = Column(
+      children: [
+        CoverImage(
+          imageUrl: imageUrl,
+        ),
+        Text(title)
+      ],
+    );
+
     return Padding(
       padding: const EdgeInsets.all(5.0),
       child: GestureDetector(
         onTap: onTap,
-        child: CustomCard(
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.bottomCenter,
-                height: 120.0,
-                width: 120.0,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                    image: imageUrl == "" ? _placeholderImage: _backgroundImage(imageUrl),
-                    fit: BoxFit.contain,
-                  )
-                ),
-              ),
-              Text(title)
-            ],
-          ),
+        child: enableCustomBackground
+            ? _withCustomBackground
+            : _withoutCustomBackground,
+      ),
+    );
+  }
+}
+
+class CoverImage extends StatelessWidget {
+  const CoverImage({
+    Key? key,
+    this.imageUrl = "",
+  }) : super(key: key);
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image:
+              imageUrl == "" ? _placeholderImage : _backgroundImage(imageUrl),
+          fit: BoxFit.contain,
         ),
       ),
     );
   }
 
-  final ImageProvider _placeholderImage = const AssetImage("assets/images/music.png");
+  final ImageProvider _placeholderImage =
+      const AssetImage("assets/images/vinyl-record-blue-bg.png");
 
-  ImageProvider _backgroundImage (url) => NetworkImage(url);
+  ImageProvider _backgroundImage(String url){
+    if(url.startsWith('http')) return NetworkImage(url);
+    return AssetImage(url);
+  }
 }
-
-
