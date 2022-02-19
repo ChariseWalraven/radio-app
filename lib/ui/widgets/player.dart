58c3c7fad5f60069de4bbf5e05819b7d46ac4119
Charise Walraven <cherri.walraven@gmail.com>
2022-02-19 01:20:30 +0100

    tweaks ui of player

--- lib/ui/widgets/player.dart
+++ lib/ui/widgets/player.dart
@@ -1,77 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:radio_app/core/constants/constants.dart';
 import 'package:radio_app/core/providers/app_state.dart';
 import 'package:radio_app/core/enums/playing_state.dart';
 import 'package:radio_app/core/providers/favourites_state.dart';
+import 'package:radio_app/ui/widgets/custom_card.dart';
+import 'package:radio_app/ui/widgets/tile.dart';
 
 class PlayerBar extends StatelessWidget {
   const PlayerBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   final double playerHeight = MediaQuery
-        .of(context)
-        .size
-        .height / 15;
-    return Consumer<AppState>(
-      builder: (context, _state, child) {
-        return SizedBox(
-          height: _state.playingState != PlayingState.none ? playerHeight : 0,
+    final double playerHeight = MediaQuery.of(context).size.height * 0.15;
+    return Consumer<AppState>(builder: (context, _state, child) {
+      return Container(
+        margin: const EdgeInsets.only(bottom: 5, right: 5, left: 5),
+        height: _state.playingState != PlayingState.none ? playerHeight : 0,
+        child: CustomCard(
+          enableShadow: false,
           child: Center(
             child: _state.playingState == PlayingState.loading
                 ? const Center(
-              child: CircularProgressIndicator(),
-            )
-                : Row(
-              children: <Widget>[
-                Expanded(
-                  child: Text(_state.title),
-                ),
-                Expanded(
-                  child: Row(
-                    children: <Widget>[
-                      _pauseButton(_state)
-                    ],
+                    child: CircularProgressIndicator(),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: CoverImage(
+                                    imageUrl: _state.station.favicon),
+                              ),
+                              Expanded(
+                                child: Text(_state.name),
+                              )
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Center(child: Text(_state.title)),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  _favouriteButton(_state, context),
+                                  _pauseButton(_state),
+                                ],
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-
-                ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      _favouriteButton(_state, context)
-                    ]
-                  ),
-                ),
-              ],
-            ),
           ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
 
   Widget _pauseButton(AppState state) {
     if (state.playingState == PlayingState.playing) {
       return IconButton(
-          onPressed: () => state.pausePlaying(),
-          icon: const Icon(Icons.pause)
+        onPressed: () => state.pausePlaying(),
+        icon: const Icon(Icons.pause_circle_filled),
+        iconSize: kIconSizeDefault,
       );
     } else if (state.playingState == PlayingState.paused) {
       return IconButton(
-          onPressed: () => state.startPlaying(),
-          icon: const Icon(Icons.play_arrow)
+        onPressed: () => state.startPlaying(),
+        icon: const Icon(Icons.play_circle),
+        iconSize: kIconSizeDefault,
       );
     }
     return Container();
   }
 
   Widget _favouriteButton(AppState state, BuildContext context) {
+    void _handleOnPress() async {
+      await state.toggleFavourite(state.station.stationuuid);
+      Provider.of<FavouritesState>(context, listen: false)
+            .refreshFavourites();
+    }
     return IconButton(
-      onPressed: () {
-        state.toggleFavourite(state.station.stationuuid);
-        Provider.of<FavouritesState>(context, listen: false).refreshFavourites();
-      },
-      icon: Icon(state.station.isFavourite ? Icons.favorite : Icons.favorite_border)
+      onPressed: _handleOnPress,
+      icon: Icon(
+          state.station.isFavourite ? Icons.favorite : Icons.favorite_border),
     );
   }
 }
