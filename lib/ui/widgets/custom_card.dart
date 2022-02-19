de0b15add9ea3ea8c402eac0f98212b7e389f261
Charise Walraven <cherri.walraven@gmail.com>
2022-02-19 01:20:13 +0100

    adds some configuration params

--- lib/ui/widgets/custom_card.dart
+++ lib/ui/widgets/custom_card.dart
@@ -5,15 +5,19 @@ class CustomCard extends StatelessWidget {
   const CustomCard({
     Key? key,
     this.borderColor,
+    this.shadowColor,
     this.child,
     this.shadowSize = 10,
     this.cornerRadius = 20,
+    this.enableShadow = true,
   }) : super(key: key);
 
   final Color? borderColor;
   final double borderWidth = 4.0;
   final double shadowSize;
   final Widget? child;
+  final Color? shadowColor;
+  final bool enableShadow;
 
   final double cornerRadius;
   final double cornerSmoothing = 0.6;
@@ -25,8 +29,9 @@ class CustomCard extends StatelessWidget {
     final ColorScheme colorScheme = theme.colorScheme;
     final Color _borderColor = borderColor ?? colorScheme.onBackground;
     final Color _backgroundColor = colorScheme.background;
+    final Color _shadowColor = shadowColor ?? colorScheme.secondary;
 
-    return _card(colorScheme.secondary, _backgroundColor, _borderColor);
+    return _card(_shadowColor, _backgroundColor, _borderColor);
   }
 
   Widget _card(Color shadowColor, Color backgroundColor, Color borderColor) {
@@ -37,54 +42,89 @@ class CustomCard extends StatelessWidget {
       ),
     );
     return LayoutBuilder(builder: (context, configuration) {
-      double shadowHeight = configuration.maxHeight - borderWidth;
+      double shadowHeight = configuration.maxHeight > 0
+          ? configuration.maxHeight - borderWidth
+          : configuration.maxHeight;
       double shadowWidth = configuration.maxWidth - borderWidth - 10;
 
-      return Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            width: shadowWidth,
+      List<Widget> _withShadow = [
+        Container(
+          width: shadowWidth,
+          decoration: ShapeDecoration(
+            shape: _shape,
+            color: borderColor,
+          ),
+        ),
+        Container(
+          height: shadowHeight,
+          width: shadowWidth,
+          decoration: ShapeDecoration(
+            shape: _shape,
+            color: shadowColor,
+          ),
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: EdgeInsets.only(top: borderWidth / 2),
+            height: shadowHeight > 0 ? shadowHeight - shadowSize : shadowHeight,
+            width: configuration.maxWidth,
             decoration: ShapeDecoration(
               shape: _shape,
               color: borderColor,
             ),
           ),
-          Container(
-            height: shadowHeight,
-            width: shadowWidth,
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: EdgeInsets.only(top: borderWidth / 2),
+            height: shadowHeight > 0
+                ? shadowHeight - shadowSize - (borderWidth / 2)
+                : shadowHeight,
+            width: configuration.maxWidth - borderWidth,
             decoration: ShapeDecoration(
               shape: _shape,
-              color: shadowColor,
+              color: backgroundColor,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: child ?? const SizedBox(),
             ),
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              margin: EdgeInsets.only(top: borderWidth/2),
-                height: shadowHeight - shadowSize,
-                width: configuration.maxWidth,
-                decoration: ShapeDecoration(
-                  shape: _shape,
-                  color: borderColor,
-                ),
-              ),
+        ),
+      ];
+
+      List<Widget> _withoutShadow = [
+        Container(
+          height: configuration.maxHeight,
+          width: configuration.maxWidth,
+          decoration: ShapeDecoration(
+            shape: _shape,
+            color: borderColor,
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              margin: EdgeInsets.only(top: borderWidth/2),
-                height: shadowHeight - shadowSize - (borderWidth/2),
-                width: configuration.maxWidth - borderWidth,
-                decoration: ShapeDecoration(
-                  shape: _shape,
-                  color: backgroundColor,
-                ),
-                child: child ?? const SizedBox(),
-              ),
+        ),
+        Container(
+          height: configuration.maxHeight > 0
+              ? configuration.maxHeight - borderWidth
+              : configuration.maxHeight,
+          width: configuration.maxWidth > 0
+              ? configuration.maxWidth - borderWidth
+              : configuration.maxWidth,
+          decoration: ShapeDecoration(
+            shape: _shape,
+            color: backgroundColor,
           ),
-          
-        ],
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: child ?? const SizedBox(),
+          ),
+        ),
+      ];
+
+      return Stack(
+        alignment: Alignment.center,
+        children: enableShadow ? _withShadow : _withoutShadow,
       );
     });
   }
