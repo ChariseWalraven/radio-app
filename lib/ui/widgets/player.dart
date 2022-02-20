283e3190dd056a8f4fb511ebab91f88e13196f04
Charise Walraven <cherri.walraven@gmail.com>
2022-02-20 22:50:30 +0100

    refactors player; implements remove button

--- lib/ui/widgets/player.dart
+++ lib/ui/widgets/player.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:radio_app/core/constants/constants.dart';
 import 'package:radio_app/core/providers/app_state.dart';
 import 'package:radio_app/core/enums/playing_state.dart';
-import 'package:radio_app/core/providers/favourites_state.dart';
-import 'package:radio_app/model/station/station.dart';
 import 'package:radio_app/ui/widgets/custom_card.dart';
-import 'package:radio_app/ui/widgets/tile.dart';
+import 'package:radio_app/ui/widgets/widgets.dart';
 
 class PlayerBar extends StatelessWidget {
   const PlayerBar({Key? key}) : super(key: key);
@@ -17,126 +14,22 @@ class PlayerBar extends StatelessWidget {
   }
 }
 
-Widget _pauseButton(BuildContext context) {
-  AppState state = context.watch<AppState>(); 
-  if (state.playingState == PlayingState.playing) {
-    return IconButton(
-      onPressed: () => state.pausePlaying(),
-      icon: const Icon(Icons.pause_circle_filled),
-      iconSize: kIconSizeDefault,
-    );
-  } else if (state.playingState == PlayingState.paused) {
-    return IconButton(
-      onPressed: () => state.startPlaying(),
-      icon: const Icon(Icons.play_circle),
-      iconSize: kIconSizeDefault,
-    );
-  }
-  return Container();
-}
-
-Widget _favouriteButton(BuildContext context) {
-  AppState state = context.watch<AppState>();
-  void _handleOnPress() async {
-    await state.toggleFavourite(state.station.stationuuid);
-    Provider.of<FavouritesState>(context, listen: false).refreshFavourites();
-  }
-
-  return IconButton(
-    onPressed: _handleOnPress,
-    icon: Icon(
-        state.station.isFavourite ? Icons.favorite : Icons.favorite_border),
-  );
-}
-
-class StationNameAndImage extends StatelessWidget {
-  const StationNameAndImage({
-    Key? key, 
-    required this.stationFaviconUrl, 
-    required this.stationName,
-    required this.stationPlaceholderImagePath,
-  }) : super(key: key);
-
-  final String stationFaviconUrl;
-  final String stationPlaceholderImagePath;
-  final String stationName;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-          flex: 3,
-          child: CoverImage(
-            imageUrl: stationFaviconUrl,
-            placeholderImagePath: stationPlaceholderImagePath,
-          ),
-        ),
-        Text(
-          stationName,
-          maxLines: 4,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 11 * MediaQuery.of(context).textScaleFactor,
-            overflow: TextOverflow.ellipsis,
-          ),
-        )
-      ],
-    );
-  }
-}
-
 const Widget _loadingIndicator = Center(child: CircularProgressIndicator());
 
 Widget _playerBuilder(BuildContext context, AppState _state, Widget? _) {
   final double playerHeight = MediaQuery.of(context).size.height * 0.15;
 
   final PlayingState playingState = _state.playingState;
-  final Station station = _state.station;
-  final String name = _state.name;
 
   Widget _child = _loadingIndicator;
 
-  if (playingState != PlayingState.loading) {
+  if(playingState != PlayingState.loading) {
     _child = Padding(
       padding: const EdgeInsets.all(10.0),
       child: Row(
-        children: <Widget>[
-          Expanded(
-            child: StationNameAndImage(
-              stationFaviconUrl: station.favicon,
-              stationName: name,
-              stationPlaceholderImagePath: station.placeholderFavicon,
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      _state.title,
-                      maxLines: 3,
-                      softWrap: true,
-                      style: TextStyle(
-                        overflow: TextOverflow.ellipsis,
-                        fontSize: 12 * MediaQuery.of(context).textScaleFactor,
-                      ),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    _favouriteButton(context),
-                    _pauseButton(context),
-                  ],
-                )
-              ],
-            ),
-          )
+        children: const [
+          Expanded(child: StationNameAndImage()),
+          Expanded(flex: 3, child: SongNameAndPlayerButtonBar()),
         ],
       ),
     );
---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,158 @@
+// ignore_for_file: implementation_imports
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:radio_app/core/constants/constants.dart';
+import 'package:radio_app/core/enums/playing_state.dart';
+import 'package:radio_app/core/providers/app_state.dart';
+import 'package:radio_app/core/providers/favourites_state.dart';
+import 'package:radio_app/model/station/station.dart';
+import 'package:radio_app/ui/widgets/tile.dart';
+
+class PlayPauseButton extends StatelessWidget {
+  const PlayPauseButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    AppState state = context.watch<AppState>();
+    Widget _playPauseButton = Container();
+
+    if (state.playingState == PlayingState.playing) {
+      _playPauseButton = IconButton(
+        onPressed: state.pausePlaying,
+        icon: const Icon(Icons.pause_circle_filled),
+        iconSize: kIconSizeDefault,
+      );
+    } else if (state.playingState == PlayingState.paused) {
+      _playPauseButton = IconButton(
+        onPressed: state.startPlaying,
+        icon: const Icon(Icons.play_circle),
+        iconSize: kIconSizeDefault,
+      );
+    }
+    return _playPauseButton;
+  }
+}
+
+class FavouriteButton extends StatelessWidget {
+  const FavouriteButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    AppState state = context.watch<AppState>();
+    void _handleOnPress() async {
+      await state.toggleFavourite(state.station.stationuuid);
+      context.read<FavouritesState>().refreshFavourites();
+    }
+
+    return IconButton(
+      onPressed: _handleOnPress,
+      icon: Icon(
+          state.station.isFavourite ? Icons.favorite : Icons.favorite_border),
+    );
+  }
+}
+
+class RemoveButton extends StatelessWidget {
+  const RemoveButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: const Text('Remove', style: TextStyle(color: Colors.red),),
+      onPressed: context.watch<AppState>().removeAndBlacklistStream,
+    );
+  }
+}
+
+class PlayerButtonBar extends StatelessWidget {
+  const PlayerButtonBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _children = [
+      const PlayPauseButton(),
+      const FavouriteButton(),
+    ];
+    if (context.watch<AppState>().playingState == PlayingState.errored) {
+      _children = [
+        const RemoveButton(),
+      ];
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: _children,
+    );
+  }
+}
+
+class SongNameAndPlayerButtonBar extends StatelessWidget {
+  const SongNameAndPlayerButtonBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String title = context.watch<AppState>().title;
+    PlayingState playingState = context.watch<AppState>().playingState;
+
+    String? _errorMessage;
+
+    if (playingState == PlayingState.errored) {
+      _errorMessage =
+          'Cannot play $title. Remove this station to prevent it from appearing.';
+    }
+
+    return Column(
+      children: [
+        Expanded(
+          child: Center(
+            child: Text(
+              _errorMessage ?? title,
+              maxLines: 3,
+              softWrap: true,
+              style: TextStyle(
+                overflow: TextOverflow.ellipsis,
+                fontSize: 12 * MediaQuery.of(context).textScaleFactor,
+              ),
+            ),
+          ),
+        ),
+        const PlayerButtonBar(),
+      ],
+    );
+  }
+}
+
+class StationNameAndImage extends StatelessWidget {
+  const StationNameAndImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    AppState state = context.watch<AppState>();
+    Station station = state.station;
+    PlayingState playingState = state.playingState;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          flex: 3,
+          child: CoverImage(
+            imageUrl: station.favicon,
+            placeholderImagePath: station.placeholderFavicon,
+          ),
+        ),
+        Text(
+          playingState != PlayingState.errored ? station.name : "ERROR",
+          maxLines: 4,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 11 * MediaQuery.of(context).textScaleFactor,
+            overflow: TextOverflow.ellipsis,
+          ),
+        )
+      ],
+    );
+  }
+}
