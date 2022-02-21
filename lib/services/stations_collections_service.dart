cbb0008f39798a01afe7bc848e1bee0dff7815b2
Charise Walraven <cherri.walraven@gmail.com>
2022-02-21 02:08:09 +0100

    cleans up

--- lib/services/stations_collections_service.dart
+++ lib/services/stations_collections_service.dart
@@ -8,33 +8,50 @@ import 'package:lingo_jam/services/locale_service.dart';
 // get languages when the app starts up (before you want to access the list in the home screen) possibly in state init
 
 class StationsCollectionsService {
-
-  static final List<String> _defaultLanguages = ['chinese', 'english', 'spanish', 'german', 'arabic', 'hindi', 'bengali', 'indonesian', 'french', 'russian', 'portuguese'];
+  static final List<String> _defaultLanguages = [
+    'chinese',
+    'english',
+    'spanish',
+    'german',
+    'arabic',
+    'hindi',
+    'bengali',
+    'indonesian',
+    'french',
+    'russian',
+    'portuguese'
+  ];
   static final List<StationsCollectionService> _collections = [];
 
   static final LocaleService _localeService = LocaleService();
 
   static List<StationsCollectionService> get collections => _collections;
-  static StationsCollectionService collection(String stationTitle) => _getStationCollectionByTitle(stationTitle);
+  static StationsCollectionService collection(String stationTitle) =>
+      _getStationCollectionByTitle(stationTitle);
 
-  static StationsCollectionService _getStationCollectionByTitle(String stationTitle) {
-    return _collections.firstWhere((collection) => collection.title == stationTitle);
+  static StationsCollectionService _getStationCollectionByTitle(
+      String stationTitle) {
+    return _collections
+        .firstWhere((collection) => collection.title == stationTitle);
   }
 
   static Future<List<StationsCollectionService>> populateCollections() async {
-    for(var language in _defaultLanguages) {
-      var languageISOCode = await _localeService.getISOLocaleByLanguageName(language);
-      var filter = StationsFilter( 
-        language: languageISOCode.name.toLowerCase(), 
-        order: Order.clickCount, 
-        reverse: true
-      );
+    for (var language in _defaultLanguages) {
+      var languageISOCode =
+          await _localeService.getISOLocaleByLanguageName(language);
+      var filter = StationsFilter(
+          language: languageISOCode.name.toLowerCase(),
+          order: Order.clickCount,
+          reverse: true);
 
       String title = languageISOCode.endonym;
 
-      if(languageISOCode.endonym.toLowerCase() != 'english') title += ' (${languageISOCode.name})';
+      if (languageISOCode.endonym.toLowerCase() != 'english') {
+        title += ' (${languageISOCode.name})';
+      }
 
-      var stationCollection = StationsCollectionService(title: title, filter: filter);
+      var stationCollection =
+          StationsCollectionService(title: title, filter: filter);
 
       await stationCollection.stations;
       _collections.add(stationCollection);
