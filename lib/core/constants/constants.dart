25008860bce29de2e55ef23be934ad3136514f91
Charise Walraven <cherri.walraven@gmail.com>
2022-02-24 01:04:47 +0100

    adds blacklisted hosts functionality

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -4,3 +4,6 @@ import 'package:lingo_jam/model/station/stations_filter.dart';
 const String kAppName = "Radio App";
 const StationsFilter kDefaultStationsFilter = StationsFilter();
 Station kDefaultStation = Station(stationuuid: "", name: "", url: "", urlResolved: "", homepage: "", favicon: "", tags: "", bitrate: 0, clickCount: 0);
+const List<String> kBlacklistedHosts = [
+  'funkyimg.com'
+];
