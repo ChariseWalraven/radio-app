3436a1b2d90056d720110484a3c5284e90b2a48a
Charise Walraven <cherri.walraven@gmail.com>
2022-02-24 01:26:23 +0100

    sets preferred orientations

--- lib/lingo_jam.dart
+++ lib/lingo_jam.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:lingo_jam/core/constants/constants.dart';
 import 'package:lingo_jam/services/routing_service.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,7 @@ class LingoJam extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
     return MaterialApp(
       initialRoute: RoutingService.home,
       routes: RoutingService.routes,
